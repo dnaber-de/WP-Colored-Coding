fd9de522f3c8159d7789f1096f090e99da0be980
David Naber <kontakt@dnaber.de>
2012-06-19 21:16:44 +0200

    more license stuff

--- wp-colored-coding.php
+++ wp-colored-coding.php
@@ -7,30 +7,29 @@
  * Version:     0.1
  * Description: Managing Codeblocks independent from the WP Texteditor and use Rainbow.js for syntax highlighting.
  * Textdomain:  wp-cc
- * License:     MIT
- * License URI: http://www.opensource.org/licenses/mit-license.php
+ * License:     Apache 2.0
+ * License URI: http://www.apache.org/licenses/LICENSE-2.0
  *
- * The license includes NOT the Rainbow.js script. Its licence (Apache License 2.0) can be found in js/rainbow/licence.txt
+ * Copyright 2012 David Naber
  *
- * Copyright (c) 2012 David Naber
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This package distributes a copy of Rainbow.js
+ * which is also licensed under the Apache License, Version 2.0
+ *
+ * Copyright 2012 Craig Campbell
+ *
+ * see js/rainbow/LICENSE for more information
  */
 
 if ( ! function_exists( 'add_filter' ) )
